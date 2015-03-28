fcc43027211c00201de4612c9fb80af1902f5df8
Joel Panlaqui <j_panlaqui@outlook.com>
2015-03-28 10:34:59 -0400

    UI improvement 1

--- PowerCycling/PowerCycling.Designer.cs
+++ PowerCycling/PowerCycling.Designer.cs
@@ -286,7 +286,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(566, 301);
+            this.ClientSize = new System.Drawing.Size(566, 295);
             this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
